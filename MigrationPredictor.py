ac500f88d76fccfb69cfae85e63257a14d7a63db
jeromepellet <jerome.pellet@nplusp.ch>
2026-02-04 12:06:07 +0100

    Update MigrationPredictor.py

--- MigrationPredictor.py
+++ MigrationPredictor.py
@@ -211,8 +211,9 @@ try:
             
             * **A venir**
            
-            ### Ressources et Données Suisses
+            ### Ressources et données en Suisse
             * **Info Fauna karch (2026).** *Base de données sur les voies de migration en Suisse (ZSDB)*. [https://lepus.infofauna.ch/zsdb](https://lepus.infofauna.ch/zsdb)
+            * **Conflits liés au trafic (2026). [https://map.geo.admin.ch](https://s.geo.admin.ch/cwvc8ynhjv0j)
             """)
 
 except Exception as e:

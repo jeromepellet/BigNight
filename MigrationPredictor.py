df7d5632a9a95599c2d2102472827988c5b9801d
jeromepellet <jerome.pellet@nplusp.ch>
2026-02-04 17:55:28 +0100

    Update MigrationPredictor.py

--- MigrationPredictor.py
+++ MigrationPredictor.py
@@ -283,8 +283,9 @@ try:
             st.markdown("""
             L'activité migratoire des amphibiens est un phénomène multi-factoriel. Ce radar utilise une approche basée sur la synergie entre les seuils physiologiques et les déclencheurs environnementaux.
             ### Paramètrage
-            * **Seuil thermique :** En deçà de **4°C**, le métabolisme ralentit. Le modèle réduit drastiquement toute prévision d'activité sous ce seuil.
-            * **Synergie thermo-hydrique :** Le modèle utilise une fonction multiplicative : le score thermique est plafonné par l'humidité. Un sol sec réduit la probabilité, même par grande douceur.
+            * **Seuils :** En deçà de **4°C**, le métabolisme ralentit. Le modèle réduit drastiquement toute prévision d'activité sous ce seuil, de même que si les conditions deviennent soudainement plus sèches après une pluie.
+            * **Inertie  :** Le modèle s'appuie en partie sur la température moyenne et la pluviométrie des 8 dernières heures.
+            * **Synergie  :** Le modèle utilise une fonction multiplicative : le score thermique est plafonné par l'humidité. Un sol sec réduit la probabilité, même par grande douceur.
             * **Influence lunaire :** Agit comme un synchronisateur. Les pics sont souvent observés aux abords de la pleine lune.
 
             ### Références
